4869612d3e7ade7a65c64a5ae303d4d89a1da679
Bianka Martinovic <info@webbird.de>
2012-12-07 12:25:24 +0100

    update to jQuery UI 1.9.2, Core 1.8.3

--- jquery-ui/external/jquery.metadata.js
+++ jquery-ui/external/jquery.metadata.js
@@ -7,8 +7,6 @@
  *   http://www.opensource.org/licenses/mit-license.php
  *   http://www.gnu.org/licenses/gpl.html
  *
- * Revision: $Id: jquery.metadata.js 1430 2011-11-29 10:39:15Z webbird $
- *
  */
 
 /**
